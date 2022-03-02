626fd48b2ed8500c1cdee2af2541ff14952948ac
Antonio L <antonio.mail@gmail.com>
2022-03-02 17:53:32 +0100

    routes

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+
+import * as parserRoutes from './components/parser/parser.routes';
+
+const router = express.Router();
+
+router.use(`/${parserRoutes.mapping}`, parserRoutes.router);
+
+router.get(`/health`, (req, res) =>
+    res.status(200).json({ ok: true, timestamp: new Date().getTime() })
+);
+
+
+export { router };
