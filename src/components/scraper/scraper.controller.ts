0461847108101d8af18081c57f44f9f2d5be6ed0
Antonio L <antonio.mail@gmail.com>
2022-03-03 20:56:08 +0100

    check if data exists

--- src/components/scraper/scraper.controller.ts
+++ src/components/scraper/scraper.controller.ts
@@ -1,17 +1,22 @@
-import * as GlassDoorService from './glassdoor/glassdoor.service';
+import ScraperQueue from './scraper.queue';
 import express from 'express';
 import logger from '../../logger';
+
+import * as ScraperDataRepository from './scraperData.repository';
 
 export async function scrapeUserProfileDataController(
   req: express.Request,
   res: express.Response
 ): Promise<express.Response> {
   const { username, password } = req.body;
+  const { f } = req.query;
+  const force = f === 'true';
 
-  //check does data for that username exists
-  //check expiration date
+  let data = await ScraperDataRepository.getScrapedData(username);
+  if (force || !data || ScraperDataRepository.checkIfScrapingDataExpiries(username)) {
+    data = await ScraperDataRepository.createScrapingData(username);
+    await ScraperQueue.add({ username, password });
+  }
 
-  const data = await GlassDoorService.fetchUserProfileData(username, password);
-
-  return res.status(200).json({ ok: true, data });
+  return res.status(200).json({ data });
 }
