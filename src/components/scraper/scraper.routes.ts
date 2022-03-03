e804798c589730405b36721276e8c24a26c1c591
Antonio L <antonio.mail@gmail.com>
2022-03-03 12:26:31 +0100

    import path fix

--- src/components/scraper/scraper.routes.ts
+++ src/components/scraper/scraper.routes.ts
@@ -1,10 +1,11 @@
-import * as express from 'express';
+import express from 'express';
 import asyncHandler from 'express-async-handler';
 import * as parserController from './scraper.controller';
 
 const mapping = 'scraper';
 const router = express.Router();
 
+// @ts-ignore
 router.post('/glassdoor/user', asyncHandler(parserController.parseUserProfileDataController));
 
 export { router, mapping };
