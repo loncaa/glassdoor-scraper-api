9e2ac57de7476b24c48366014063fe340e00eb6c
Antonio L <antonio.mail@gmail.com>
2022-03-03 12:25:43 +0100

    consume service method

--- src/components/scraper/scraper.controller.ts
+++ src/components/scraper/scraper.controller.ts
@@ -1,4 +1,4 @@
-import * as ParserService from './scraper.service';
+import * as GlassDoorService from './glassdoor/glassdoor.service';
 import express from 'express';
 import logger from '../../logger';
 
@@ -8,5 +8,10 @@ export async function parseUserProfileDataController(
 ): Promise<express.Response> {
   const { username, password } = req.body;
 
-  return res.status(200).json({ ok: true });
+  //check does data for that username exists
+  //check expiration date
+
+  const data = await GlassDoorService.fetchUserProfileData(username, password);
+
+  return res.status(200).json({ ok: true, data });
 }
