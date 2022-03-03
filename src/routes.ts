3e32bf5a69f728b0637f171f17d9086db11780dc
Antonio L <antonio.mail@gmail.com>
2022-03-03 17:24:11 +0100

    add user profile route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 
-import * as parserRoutes from './components/scraper/scraper.routes';
+import * as ParserRoutes from './components/scraper/scraper.routes';
+import * as UserProfileRoutes from './components/userProfile/usedProfile.routes';
 
 const router = express.Router();
 
-router.use(`/${parserRoutes.mapping}`, parserRoutes.router);
+router.use(`/${ParserRoutes.mapping}`, ParserRoutes.router);
+router.use(`/${UserProfileRoutes.mapping}`, UserProfileRoutes.router);
 
 router.get(`/health`, (req, res) =>
   res.status(200).json({ ok: true, timestamp: new Date().getTime() })
