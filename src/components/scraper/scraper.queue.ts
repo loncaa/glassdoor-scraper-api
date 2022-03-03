90bc91a3b059c2e847b7a14ba28c9039940211eb
Antonio L <antonio.mail@gmail.com>
2022-03-03 20:55:55 +0100

    implement queue

---
+++ src/components/scraper/scraper.queue.ts
@@ -0,0 +1,37 @@
+import Queue from 'bull';
+
+import * as GlassDoorScraper from './glassdoor/glassdoor.scraper';
+import { ScrapingStatus } from './scraperData.model';
+
+import * as ScraperDataRepository from './scraperData.repository';
+import * as UserDataRepository from '../userProfile/userProfile.repository';
+
+const scraperQueue = new Queue('glassdoor scraper');
+
+scraperQueue.process(async function (job) {
+  const { username, password } = job.data;
+  return await GlassDoorScraper.scrapeUserProfileData(username, password);
+});
+
+scraperQueue.on('failed', async function (job, err) {
+  const { username } = job.data;
+  console.log(`Job ${job.id} failed ${JSON.stringify(err)}`);
+
+  const payload = {
+    status: ScrapingStatus.FAILED,
+  };
+  await ScraperDataRepository.updateScrapingData(username, payload);
+});
+
+scraperQueue.on('completed', async function (job, scrapedData) {
+  const { username } = job.data;
+  const userProfile = await UserDataRepository.createUserProfile(username, scrapedData);
+
+  const payload = {
+    status: ScrapingStatus.DONE,
+    userProfile,
+  };
+  await ScraperDataRepository.updateScrapingData(username, payload);
+});
+
+export default scraperQueue;
