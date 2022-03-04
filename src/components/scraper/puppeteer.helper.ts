fff431054b2959a2511d58589c736712aee3b1af
Antonio L <antonio.mail@gmail.com>
2022-03-04 11:25:31 +0100

    add headless puppeteer options

--- src/components/scraper/puppeteer.helper.ts
+++ src/components/scraper/puppeteer.helper.ts
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 export async function createBrowser(): Promise<puppeteer.Browser> {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox'],
   });
 
   return browser;
